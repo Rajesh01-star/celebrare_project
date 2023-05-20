2b568f2ede8029ce896dede578c082dc0eeeba6e
Rajesh01-star <rajeshbehera489@gmail.com>
2023-05-20 17:42:22 +0530

    all clear

--- app/components/Model.js
+++ app/components/Model.js
@@ -1,31 +1,131 @@
-"use client"
-import {RxCross2} from "react-icons/rx"
+import { RxCross2 } from "react-icons/rx";
+import { useState } from "react";
 
-import { useState } from "react"
+export default function Modal(props) {
+  const [mask, setMask] = useState("");
+  const [imageSrc, setImageSrc] = useState(props.imgSrc);
+  const [showModal, setShowModal] = useState(true);
 
-export default function Modal(props){
-    const [mask,setMask] = useState("")
-    const [imageSrc,setImageSrc] = useState(props.imgSrc)
-
-    const handleClick = (e)=>{
-        const mask = e.target.id;
-        console.log(e.target.id);
+  const handleClick = (e) => {
+    const selectedMask = e.target.id;
+    setMask(selectedMask);
+    if (selectedMask === "original") {
+      // If the "Original" button is selected, show the original image without any mask
+      setImageSrc(props.imgSrc);
+    } else {
+      // Apply the selected mask on top of the main image
+      const maskImageSrc = `/${selectedMask}.png`; // Assuming the mask images are named as "mask-1.png", "mask-2.png", etc.
+      const image = new Image();
+      image.src = imageSrc;
+      image.onload = function () {
+        const canvas = document.createElement("canvas");
+        canvas.width = image.width;
+        canvas.height = image.height;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(image, 0, 0);
+        const maskImage = new Image();
+        maskImage.src = maskImageSrc;
+        maskImage.onload = function () {
+          ctx.globalCompositeOperation = "destination-in";
+          ctx.drawImage(maskImage, 0, 0, canvas.width, canvas.height);
+          setImageSrc(canvas.toDataURL());
+        };
+      };
     }
-    return(
-        <div className="container bg-white w-[80%] mt-10 mx-auto h-[30rem] pt-10 relative">
-            <h1 className="text-center mb-4">Uploaded image</h1>
-            <RxCross2 className="absolute top-6 right-6 text-3xl cursor-pointer"/>
-            <img src={imageSrc} alt="uploaded image" className="w-[20rem] mx-auto" mask={mask}/> 
-            <div className="flex justify-center gap-4 mt-5">
-            <button className="border-2 border-gray-400 rounded-xl p-4">Original</button>
-            <img id="1" src="/1.png" className="w-[4rem] border-2 border-gray-400 rounded-xl p-4 cursor-pointer" onClick={handleClick}/>
-            <img id="2" src="/2.png" className="w-[4rem] border-2 border-gray-400 rounded-xl p-4 cursor-pointer" onClick={handleClick}/> 
-            <img id="3" src="/3.png" className="w-[4rem] border-2 border-gray-400 rounded-xl p-4 cursor-pointer" onClick={handleClick}/>
-            <img id="4" src="/4.png" className="w-[4rem] border-2 border-gray-400 rounded-xl p-4 cursor-pointer"onClick={handleClick} /> 
-            </div>
-            <div className="flex justify-center">
-                <button className="w-[80%] mt-4 h-[3.5rem] bg-[#27aaa1] rounded-xl">Use this image</button>
-            </div>
-        </div>
-    )
+  };
+
+  const handleFinalClick = ()=>{
+    props.onImageSelect(imageSrc);
+  }
+
+  const handleCloseModal = () => {
+    window.location.reload()
+  };
+
+  if (!showModal) {
+    return null; // Return null to hide the modal
+  }
+
+  return (
+    <div className="container bg-white w-full mt-10 mx-auto h-[35rem] pt-10 relative rounded-xl">
+      <h1 className="text-center mb-4">Uploaded image</h1>
+      <RxCross2
+        id="close"
+        className="absolute top-6 right-6 text-3xl cursor-pointer"
+        onClick={handleCloseModal}
+      />
+      <div className="image-container">
+        <img
+          id="main"
+          src={imageSrc}
+          alt="uploaded image"
+          className="w-[20rem] mx-auto"
+        />
+        {mask !== "original" && (
+          <div
+            className="mask-image"
+            style={{ maskImage: `url(/${mask}.png)` }}
+          />
+        )}
+      </div>
+      <div className="flex justify-center gap-4 -mt-6 px-4">
+        <button
+          id="original"
+          className="border-2 border-gray-400 rounded-xl p-4"
+          onClick={handleClick}
+        >
+          Original
+        </button>
+        <img
+          id="1"
+          src="/1.png"
+          className="w-[4rem] border-2 border-gray-400 rounded-xl p-4 cursor-pointer"
+          onClick={handleClick}
+        />
+        <img
+          id="2"
+          src="/2.png"
+          className="w-[4rem] border-2 border-gray-400 rounded-xl p-4 cursor-pointer"
+          onClick={handleClick}
+        />
+        <img
+          id="3"
+          src="/3.png"
+          className="w-[4rem] border-2 border-gray-400 rounded-xl p-4 cursor-pointer"
+          onClick={handleClick}
+        />
+        <img
+          id="4"
+          src="/4.png"
+          className="w-[4rem] border-2 border-gray-400 rounded-xl p-4 cursor-pointer"
+          onClick={handleClick}
+        />
+      </div>
+      <div className="flex justify-center">
+        <button
+          id="final"
+          className="w-[80%] mt-4 h-[3.5rem] bg-[#27aaa1] rounded-xl"
+          onClick={handleFinalClick}
+        >
+          Use this image
+        </button>
+      </div>
+      <style jsx>{`
+        .image-container {
+          position: relative;
+          width: 20rem;
+          height: 20rem;
+          margin: 0 auto;
+          overflow: hidden;
+        }
+        .mask-image {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+        }
+      `}</style>
+    </div>
+  );
 }
--- app/components/UploadButton.js
+++ app/components/UploadButton.js
@@ -1,35 +1,50 @@
-"use client"
 import { useRef, useState } from "react";
 import Modal from "./Model";
 
 const UploadButton = ({ onFileSelect }) => {
-    const inputRef = useRef(null);
-    const [image,setImage] = useState("")
+  const inputRef = useRef(null);
+  const [image, setImage] = useState("");
+  const [printImage,setPrintImage] = useState("");
 
-    const handleClick = ()=>{
-        inputRef.current.click(); 
-    }
+  const handleClick = () => {
+    inputRef.current.click();
+  };
 
-    const handleImageUpload = (event)=>{
-        setImage(event.target.files[0])
-    }
-    
+  const handleImageUpload = (event) => {
+    setImage(event.target.files[0]);
+  };
+
+  const handleImageSelect = (imageSrc) => {
+    setPrintImage(imageSrc);
+    setImage("");
+  };
+
   return (
     <div>
-    <section className="w-full h-32 rounded-lg  border-2 border-gray-300 mt-4 flex flex-col justify-center items-center">
-      <input type="file" ref={inputRef} onChange={handleImageUpload} style={{display:"none"}}/>
-      <h3>Upload image</h3>
-      <button onClick={handleClick} className="w-[50%] mt-4 h-[3.5rem] bg-[#27aaa1] rounded-xl">Choose file from storage</button>
-    </section>
-    <section>
-        {image?
-        <Modal imgSrc={URL.createObjectURL(image)} />
-        :null}
-    </section>
+      <section className="w-full h-32 rounded-lg border-2 border-gray-300 mt-4 flex flex-col justify-center items-center">
+        <input type="file" ref={inputRef} onChange={handleImageUpload} className="hidden" />
+        <h3>Upload image</h3>
+        <button
+          onClick={handleClick}
+          className="w-[50%] mt-4 h-[3.5rem] bg-[#27aaa1] rounded-xl"
+        >
+          Choose file from storage
+        </button>
+      </section>
+      {image && (
+        <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center">
+          <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 backdrop-filter backdrop-blur-sm z-10" />
+          <div className="z-20">
+          <Modal imgSrc={URL.createObjectURL(image)} onImageSelect={handleImageSelect} />
+          </div>
+        </div>
+      )}
+      {printImage ?
+      <img src={printImage} alt="final-image" className="mt-10"/>:
+      null  
+    }
     </div>
   );
 };
 
 export default UploadButton;
-
-
